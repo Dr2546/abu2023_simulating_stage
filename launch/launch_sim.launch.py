d299512c08ef706274154628613536b0a1cad081
Dr2546 <62285718+Dr2546@users.noreply.github.com>
2023-05-19 16:09:14 +0700

    Add files via upload

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,61 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    package_name = "abu2023"
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]), launch_arguments={'world':'~/ros2_ws/src/abu2023/world/abu2023.sdf'}.items() #Change Path to where the file is
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot',
+                                   '-x', "5.4",
+                                   '-y', "-0.7",
+                                   '-z', "1",],
+                        output='screen')
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
+
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    #          )
+
+    # spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    #                     arguments=['-topic', 'robot_description',
+    #                                '-entity', 'my_bot',
+    #                                '-x', "5.4",
+    #                                '-y', "-0.7",
+    #                                '-z', "1",
+    #                                '-roll', '0',
+    #                                '-pitch', '0',
+    #                                '-yaw', '0'],
+    #                     output='screen')
