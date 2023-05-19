2dee3aea37918ab3bc5b52df64af1d6e63041c97
Dr2546 <62285718+Dr2546@users.noreply.github.com>
2023-05-19 20:36:32 +0700

    Update launch_sim.launch.py

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -13,6 +13,8 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     package_name = "abu2023_simulating_stage"
+    pkg_path = os.path.join(get_package_share_directory('abu2023_simulating_stage'))
+    world_file = os.path.join(pkg_path,'world','abu2023.sdf')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -23,7 +25,7 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]), launch_arguments={'world':'~/ros2_ws/src/abu2023/world/abu2023.sdf'}.items() #Change Path to where the file is
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]), launch_arguments={'world':world_file}.items() #Change Path to where the file is
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
